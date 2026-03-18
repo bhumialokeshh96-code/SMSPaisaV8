a4d338c2e03aff63e9aab66dcaadf69c9f27a163
bhumialokeshh96-code <bhumialokeshh96@gmail.com>
2026-03-18 14:04:00 +0530

    update

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://smspaisabackend.onrender.com',
+        target: 'http://smspaisav2-env.eba-dvwrwwc7.ap-south-1.elasticbeanstalk.com',
         changeOrigin: true,
       }
     }
